1b009446224beac1a13895ccc300f52455170fc2
Wilson <wwong9948@gmail.com>
2017-01-12 06:44:07 -0500

    Player can use wep v3

--- MiniRPG/src/item/Sword.java
+++ MiniRPG/src/item/Sword.java
@@ -1,20 +1,10 @@
 package item;
 
 public class Sword extends Equipable {
-
-	private int _attack;
 	
 	public Sword(String name, int attack) {
 		super(name);
-		_attack = attack;
-	}
-	
-	public int getAttack(){
-		return _attack;
-	}
-	
-	public void setAttack(int attack){
-		_attack = attack;
+		setAttack(attack);
 	}
 
 }
